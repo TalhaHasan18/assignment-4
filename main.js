886bcbdeb05d0d8f28be659dbdd15d8512df4056
Talha Hasan18 <mtbanwar18@gmail.com>
2024-02-27 23:22:40 +0500

    fourth assignment

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+let quote = "I never dreamed about success.I worked for it";
+let author = "Estee Lauder";
+console.log(`${author} once said, "${quote}"`);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let quote: string = "I never dreamed about success.I worked for it";
+let author: string = "Estee Lauder";
+console.log(`${author} once said, "${quote}"`);
